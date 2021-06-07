2268d43fa55e2a275ddbab30fda8eeb47e2bdd99
1998-sam <85482407+1998-sam@users.noreply.github.com>
2021-06-07 17:49:34 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,21 @@
+package javaproject1;
+import java.util.Scanner;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int i = 1, factorial = 1, number;	
+    System.out.println("Enter the number:");
+	Scanner sc = new Scanner(System.in);
+	number = sc.nextInt();
+	
+	while(i<=number) {
+	factorial = factorial*i;
+	i++;
+	}
+	System.out.println("Factorial of the given number is: "+factorial);
+	
+	}
+
+}
